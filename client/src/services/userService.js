042df1026e0d3672ec8e33943ff96035c19de8e4
Alastair Lloyd <dex8056@aol.com>
2025-06-11 07:12:45 +0100

    refactor: userService to use env

--- client/src/services/userService.js
+++ client/src/services/userService.js
@@ -1,4 +1,4 @@
-const API_BASE = 'http://localhost:3000/api/users';
+const API_BASE = import.meta.env.VITE_API_BASE
 
 export const updateName = async (name, token) => {
   return fetch(`${API_BASE}/update-name`, {
